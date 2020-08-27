cbaea5129deae9f3b5adb7532a6c4249ca23bb60
Claudio Silva <claudio.silva@itspoa.com>
2020-08-27 16:01:39 -0300

    app fiori TPOV

---
+++ webapp/Component.js
@@ -0,0 +1,7 @@
+sap.ui.define(["sap/suite/ui/generic/template/lib/AppComponent"], function (AppComponent) {
+	return AppComponent.extend("itsgrp.com.zui5_fci_tpov.Component", {
+		metadata: {
+			"manifest": "json"
+		}
+	});
+});
